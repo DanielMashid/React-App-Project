c5fb2c63a054488b461f14a5ef333d0a95485e65
DanielMashid <danielmashid1@gmail.com>
2022-04-14 08:19:54 +0300

    Working with Events & React state

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,8 +1,10 @@
 import './ExpenseDate.css';
+import React from 'react';
 
-function ExpenseDate(props) {
-    const month = props.date.toLocaleString('en-US', { month: 'long' });
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+const ExpenseDate = (props) => {
+
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     const year = props.date.getFullYear();
 
     return (
